684a151a082850bafe45126bd80122cb33b86a13
Ashish chaturvedi <ashishchaturvedi758@gmail.com>
2025-03-23 22:16:51 +0530

    Add files via upload

---
+++ SQLQuery1.sql
@@ -0,0 +1,11 @@
+use [Test Table]
+
+create table TestTable
+(PatientID varchar(20)
+,PatientName varchar(25)
+,PatientState varchar(25)
+,Gender varchar(20)
+,Visits int
+,Charges int)
+
+select * from TestTable
